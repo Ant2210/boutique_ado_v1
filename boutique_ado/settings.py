ad2d5857d302bbf8758a1c78b8aae550edc0df67
Ant2210 <ant.griffiths22@gmail.com>
2023-11-19 22:46:03 +0000

    Fix stripe endpoint

--- boutique_ado/settings.py
+++ boutique_ado/settings.py
@@ -28,7 +28,7 @@ SECRET_KEY = os.environ.get('SECRET_KEY', '')
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ["8000-ant2210-boutiqueadov1-u3ywz6k9vva.ws-eu106.gitpod.io"]
 
 
 # Application definition
