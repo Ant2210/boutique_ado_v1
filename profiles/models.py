b3768f869112288c581fdb21b5d9b6f0acc25689
Ant2210 <ant.griffiths22@gmail.com>
2023-11-20 01:59:53 +0000

    CUpdated allauth templates and tested profiles

--- profiles/models.py
+++ profiles/models.py
@@ -11,10 +11,9 @@ class UserProfile(models.Model):
     A user profile model for maintaining default
     delivery information and order history
     """
-
     user = models.OneToOneField(User, on_delete=models.CASCADE)
     default_phone_number = models.CharField(max_length=20, null=True, blank=True)
-    default_country = CountryField(blank_label="Country *", null=True, blank=True)
+    default_country = CountryField(blank_label='Country *', null=True, blank=True)
     default_postcode = models.CharField(max_length=20, null=True, blank=True)
     default_town_or_city = models.CharField(max_length=40, null=True, blank=True)
     default_street_address1 = models.CharField(max_length=80, null=True, blank=True)
@@ -30,8 +29,7 @@ def create_or_update_user_profile(sender, instance, created, **kwargs):
     """
     Create or update the user profile
     """
-
-    if created: 
-        UserProfile.object.create(user=instance)
+    if created:
+        UserProfile.objects.create(user=instance)
     # Existing users: just save the profile
     instance.userprofile.save()
