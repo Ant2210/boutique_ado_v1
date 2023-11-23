373815ca891bd31ea6493008d4c8b4dd1bdeda13
Ant2210 <ant.griffiths22@gmail.com>
2023-11-23 01:53:34 +0000

    Change deployment settings

--- boutique_ado/settings.py
+++ boutique_ado/settings.py
@@ -11,6 +11,7 @@ https://docs.djangoproject.com/en/3.2/ref/settings/
 """
 
 from pathlib import Path
+import dj_database_url
 import os
 if os.path.isfile('env.py'):
     import env  # NOQA
@@ -28,7 +29,7 @@ SECRET_KEY = os.environ.get('SECRET_KEY', '')
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = ["8000-ant2210-boutiqueadov1-s8xqq0wpwag.ws-eu106.gitpod.io"]
+ALLOWED_HOSTS = ["8000-ant2210-boutiqueadov1-s8xqq0wpwag.ws-eu106.gitpod.io", "ant-boutique-ado-f57d0323e4d7.herokuapp.com", 'localhost']
 
 
 # Application definition
@@ -127,12 +128,17 @@ WSGI_APPLICATION = 'boutique_ado.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/3.2/ref/settings/#databases
 
-DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+if 'DATABASE_URL' in os.environ:
+    DATABASES = {
+        'default': dj_database_url.parse(os.environ.get('DATABASE_URL'))
     }
-}
+else:
+    DATABASES = {
+        'default': {
+            'ENGINE': 'django.db.backends.sqlite3',
+            'NAME': BASE_DIR / 'db.sqlite3',
+        }
+    }
 
 
 # Password validation
